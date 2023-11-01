073554f5ec376035c08a7ad744a9d94ea71225ed
maximo cavero <cavero.maximo.l@gmail.com>
2023-11-01 17:21:58 -0300

    terminando lo del css en favorites

--- src/views/Favorites.jsx
+++ src/views/Favorites.jsx
@@ -1,11 +1,9 @@
 import Gallery from "../components/Gallery";
 const Favorites = ({ fav }) => {
   return (
-    <div className="app">
+    <div className="App">
       <h1>Fotos favoritas</h1>
-      <div className="p-3 gallery grid-columns-4">
         <Gallery fav={true} />
-      </div>
     </div>
   );
 };
